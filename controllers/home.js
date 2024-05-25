7e6a88a36a8ce0e584e3bdfd8a24c1d59cc9c0da
Melaniemsc <melsan1706@gmail.com>
2024-05-25 19:01:43 +0100

    books can be added to the users list

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 const Books = require('../models/books.js');
+const session = require('express-session');
 
 
 router.get('/', async (req,res) =>{
@@ -12,8 +13,16 @@ router.get('/', async (req,res) =>{
 })
 
 router.get('/:bookId', async (req,res) =>{
+    // !try add error message canot find book
     const book = await Books.findById(req.params.bookId)
     res.render('show.ejs', {book})
+})
+
+router.post('/:bookId', async (req,res) =>{
+    const user = await User.findById(req.session.user.userId)
+    await user.booksAdded.push(req.params.bookId)
+    await user.save()
+    res.send("lles, this work")
 })
 
 
--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require ("mongoose");
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true},
     password: { type: String, required: true},
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    booksAdded: [{type: mongoose.Schema.ObjectId, ref: 'Book'}]
 })
 
 const User = mongoose.model("User", userSchema);
